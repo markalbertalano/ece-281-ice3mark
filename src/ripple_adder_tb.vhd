4ada41f2fae59376e8dfc36f1f0e4f125245f16f
markalbertalano <markalbertalano@users.noreply.github.com>
2025-02-11 14:47:25 -0700

    ice3.1

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -44,13 +44,13 @@ begin
 	
 	   -- Test all zeros input
 	   w_addends <= x"00"; w_Cin <= '0'; wait for 10 ns;
-	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
+	       assert (w_sum = x"00" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
+	       assert (w_sum = x"0F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
        w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"1" and w_Cout = '1') report "failure on mid case" severity failure;
+	       assert (w_sum = x"01" and w_Cout = '1') report "failure on mid case" severity failure;
        w_addends <= x"0A"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"0A" and w_Cout = '1') report "failure on rndm case" severity failure;
 		wait; -- wait forever
